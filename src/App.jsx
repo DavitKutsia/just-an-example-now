dfcd94fa3f8161017817def03350f1b2f6313515
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-13 16:59:25 +0400

    Refactor App component with navigation and content updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
-import { useState } from 'react'
-import './index.css'
+import { useRef } from 'react';
+import './index.css';
 
 function App() {
+  const personInfoRef = useRef(null);
+  const contactInfoRef = useRef(null);
+
+  const handleNavigation = (section) => {
+    const ref = section === 'personInfo' ? personInfoRef : contactInfoRef;
+    ref.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
 
   return (
     <div id='VisualizationDiv'>
       <div id="mainDiv">
         <header id='header'>
-            <img src="./public/siteIcon.jpeg" width={100} id='siteIcon'/>
-            <h1>Example Title</h1>
+          <img src="/siteIcon.jpeg" width={100} id='siteIcon' alt="Site Icon"/>
+          <h1 id='headerTitle'>Example Title</h1>
         </header>
         <div id='contentDiv'>
           <div id='someDiv1'>
             <div id='mainInfoDiv'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repudiandae in ratione voluptatem dolores enim laborum illum, 
-            aspernatur tempora nostrum delectus provident ipsum ea harum cum est. 
-            Aperiam similique doloribus laboriosam facere, 
-            nemo dolorum id necessitatibus eum eligendi non. 
-            Vitae dolorum architecto doloribus omnis laboriosam est eum nam? 
-            Possimus, maxime blanditiis!
+              <h1 id='aboutUsTitle'>ჩვენს შესახებ</h1>
+
+              <div id='aboutUsTextDiv'>
+                <p id='aboutUsText'>
+                  ჩვენი მიზანია, რომ კლინიკაში მისვლის გარეშე,
+                  საკუთარ სახლში მიიღოთ უსაფრთხო და პროფესიონალური სამედიცინო მოვლა.
+                  მოვემსახურებით თბილისის მასშტაბით,
+                </p>
+                <p id='aboutUsText'>
+                  ყველა პროცედურას ასრულებს სერტიფიცირებული და გამოცდილი ექთანი,
+                  რომელიც მუშაობს სტანდარტული ასეპტიკისა და ჰიგიენის წესებით.
+                </p>
+
+                <div id='aboutUsTable'>
+                  <p>ჩვენთვის მთავარი არის:</p>
+                  <div id='liDiv'>
+                    <ul>
+                      <li>პაციენტის უსაფრთხოება</li>
+                      <li>სუფთა და სწორი პროცედურა</li>
+                      <li>პატივისცემა და კომფორტი</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div id='contactAndPersonInfoDiv'>
-              <div id="personInfo">
-                <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Id, numquam sed delectus asperiores at sequi laudantium atque in totam fuga.
-                </p>
-                <img src="./public/vite.svg"  />
+            <div id="personInfo" ref={personInfoRef}>
+              <h2>ექთანი – ნათია ბირჭაძე</h2>
+
+              <div id="personInfoDetails">
+                <p><span id='spanInfo'>გამოცდილება:</span> 16 წელი</p>
+                <p><span id='spanInfo'>ასაკი:</span> 40 წელი</p>
               </div>
-            <div id="contactInfo"></div>
+
+              <p id="personInfoDescription">
+                ნათია ბირჭაძე არის სერტიფიცირებული და მაღალი პასუხისმგებლობის მქონე ექთანი,
+                რომელსაც აქვს მრავალწლიანი პრაქტიკა კლინიკურ და საავადმყოფო გარემოში.
+              </p>
+            </div>
+            <div id="contactInfo" ref={contactInfoRef}></div>
           </div>
         </div>
       </div>
-    </div>      
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
